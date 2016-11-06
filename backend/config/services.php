01318d992264633d621055c5bf660ce0f72435bd
Nick Porter <e@nck.me.uk>
2016-11-06 19:19:53 +0000

    added react google sign in to fe

--- backend/config/services.php
+++ backend/config/services.php
@@ -38,7 +38,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/api/authcallback',
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
     ],
 
 ];
