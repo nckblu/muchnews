5b876c926dc9c86d3743c908e97d9fca9e2306e1
Nick Porter <e@nck.me.uk>
2016-12-24 17:34:25 +0000

    basic user registration through google

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,14 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Socialite;
+use App\User;
 
 class UserController extends Controller
 {
 	public function index(Request $request) {
-		$email = $request->input("email");
+		$accessToken = $request->input("token");
+		$user = Socialite::driver('google')->userFromToken($accessToken);
+
+		if (!User::where('email', $user->getEmail())->first()) {
+			$userEmail = $user->getEmail();
+			$userName = $user->getName();
+			$newUser = new User;
+			$newUser->name = $userName;
+			$newUser->email = $userEmail;
+			$newUser->save();
+			return response()->json([
+			    'user created' => $newUser->email,
+			]);
+		}
+		
 
 		return response()->json([
-		    'email' => $email,
+		    'email' => $user->getName(),
 		]);
 	}
 }
